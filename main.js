8f1fce0483a85c70c0723fc20b045d41a331012a
mari2gfrg <138078457+mari2gfrg@users.noreply.github.com>
2024-09-05 22:40:09 -0300

    Update main.js
    aaaaaaaa

---
+++ main.js
@@ -0,0 +1,16 @@
+var name = ` Mariane Silva`
+var number= 10
+console.log (name)
+console.log(number)
+
+function print(){
+    console.log ( `Mariane Silva`)
+}
+
+ function sum(number1, number2){
+    console.log(number1 = number2)
+ }
+print (` Mariane Caroline`)
+print ( `Marianinha`)
+sum(20, 765)
+sum(359,241)
